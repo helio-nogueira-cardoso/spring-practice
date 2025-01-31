777836cc1e63cb0821faa5d05a19c4b4535478f6
Hélio Nogueira Cardoso <rinder_omna@outlook.com>
2025-01-31 10:28:27 -0300

    Practice with application context in test context | Fixes Mockito Auto Binding and Class Path Sharing Warnings

--- src/test/java/br/com/helio/SpringFrameworkPractice/SpringFrameworkPracticeApplicationTests.java
+++ src/test/java/br/com/helio/SpringFrameworkPractice/SpringFrameworkPracticeApplicationTests.java
@@ -1,10 +1,23 @@
 package br.com.helio.SpringFrameworkPractice;
 
+import br.com.helio.SpringFrameworkPractice.controllers.MyController;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class SpringFrameworkPracticeApplicationTests {
+	private final ApplicationContext applicationContext;
+
+	public SpringFrameworkPracticeApplicationTests(ApplicationContext applicationContext) {
+		this.applicationContext = applicationContext;
+	}
+
+	@Test
+	void testGetControllerFromCtx() {
+		MyController myController = applicationContext.getBean(MyController.class);
+		System.out.println(myController.hello());
+	}
 
 	@Test
 	void contextLoads() {
