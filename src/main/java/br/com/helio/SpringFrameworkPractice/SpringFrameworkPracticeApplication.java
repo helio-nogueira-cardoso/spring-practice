45906c032535545e3c370926c8cfea2b872f3d47
Hélio Nogueira Cardoso <rinder_omna@outlook.com>
2025-01-31 09:33:32 -0300

    First commit

---
+++ src/main/java/br/com/helio/SpringFrameworkPractice/SpringFrameworkPracticeApplication.java
@@ -0,0 +1,13 @@
+package br.com.helio.SpringFrameworkPractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFrameworkPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFrameworkPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/helio/SpringFrameworkPractice/SpringFrameworkPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.helio.SpringFrameworkPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFrameworkPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
