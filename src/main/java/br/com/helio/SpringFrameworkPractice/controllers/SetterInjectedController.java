50153e2bead71ae28757cf50c6c0773d369aafc3
Hélio Nogueira Cardoso <rinder_omna@outlook.com>
2025-01-31 12:03:41 -0300

    Setter method injection autowire

--- src/main/java/br/com/helio/SpringFrameworkPractice/controllers/SetterInjectedController.java
+++ src/main/java/br/com/helio/SpringFrameworkPractice/controllers/SetterInjectedController.java
@@ -1,10 +1,14 @@
 package br.com.helio.SpringFrameworkPractice.controllers;
 
 import br.com.helio.SpringFrameworkPractice.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class SetterInjectedController {
     private GreetingService greetingService;
 
+    @Autowired
     public void setGreetingService(GreetingService greetingService) {
         this.greetingService = greetingService;
     }
