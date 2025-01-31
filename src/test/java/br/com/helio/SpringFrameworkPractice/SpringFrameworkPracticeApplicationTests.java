9157c2171991f91d280b1fcfff879e8c0a8c34e4
Hélio Nogueira Cardoso <rinder_omna@outlook.com>
2025-01-31 10:43:40 -0300

    Practice dependency injection in test context directly, also via constructor in tests, which can also be done with Autowired without extra TestConstructor annotation. That is because tests are mainly managed by JUNIT, not Spring. So either use @Autowired to tell Spring to inject dependencies, or do it via constructor but also using @TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL) annotation to tell Spring to take control an inject dependencies via constructor autowiring every field.

--- src/test/java/br/com/helio/SpringFrameworkPractice/SpringFrameworkPracticeApplicationTests.java
+++ src/test/java/br/com/helio/SpringFrameworkPractice/SpringFrameworkPracticeApplicationTests.java
@@ -4,18 +4,27 @@ import br.com.helio.SpringFrameworkPractice.controllers.MyController;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.TestConstructor;
 
 @SpringBootTest
+@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
 class SpringFrameworkPracticeApplicationTests {
 	private final ApplicationContext applicationContext;
+	private final MyController myController;
 
-	public SpringFrameworkPracticeApplicationTests(ApplicationContext applicationContext) {
+	public SpringFrameworkPracticeApplicationTests(ApplicationContext applicationContext, MyController myController) {
 		this.applicationContext = applicationContext;
+		this.myController = myController;
 	}
 
 	@Test
 	void testGetControllerFromCtx() {
 		MyController myController = applicationContext.getBean(MyController.class);
+		System.out.println(myController.hello());
+	}
+
+	@Test
+	void testGetControllerDirectlyFromInjection() {
 		System.out.println(myController.hello());
 	}
 
